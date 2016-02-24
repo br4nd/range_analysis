893e2790c6e95d969f5a8e719747901e2b501003
d4wb <brandon.dewberry@timedomain.com>
2016-02-24 07:59:16 -0600

    Added stinker threshold

--- ra.py
+++ ra.py
@@ -20,48 +20,6 @@ from pprint import pprint as pp
 sys.path.append('..')
 import lib
 
-#------------------------- plot_ranges ----------------------------------------
-def plot_ranges(RA,nodeID) :
-
-    nodeID_mask = RA['rspID'] == nodeID
-
-    t_vec = RA[nodeID_mask]['t_host']
-    t0 = RA['t_host'][0]
-    t_vec = t_vec - t0
-
-    r_vec = RA[nodeID_mask]['rmeas']
-
-    plt.plot(t_vec,r_vec,'b.')
-    plt.grid(True)
-
-#------------------------- plot_snr ----------------------------------------
-def plot_snr(RA,nodeID,color,low,high,label,pass_fail) :
-
-    nodeID_mask = RA['rspID']==nodeID
-
-    mask_pii_hi = RA[nodeID_mask]['t_stopwatch'] >= low
-    mask_pii_hi = mask_pii_hi.ravel()
-
-    mask_pii_low = RA[nodeID_mask]['t_stopwatch'] <= high
-    mask_pii_low = mask_pii_low.ravel()
-
-    mask_pii = np.logical_and(mask_pii_hi,mask_pii_low)
-
-    t0 = RA['t_host'][0]
-    t_vec = RA[nodeID_mask][mask_pii]['t_host'] - t0
-    vpk_vec = RA[nodeID_mask][mask_pii]['vpeak']
-    noise_vec = RA[nodeID_mask][mask_pii]['noise']
-    snr_vec = 20.*np.log10(np.divide(vpk_vec,noise_vec))
-
-    lineH, = plt.plot(t_vec,snr_vec,color,
-            hold=True, linestyle='None', marker='o', markersize=6,
-            markerfacecolor=color, markeredgecolor=color, label=label)
-    if t_vec.size > 0 :
-        plt.plot([min(t_vec),max(t_vec)],[pass_fail,pass_fail],'k-')
-    plt.grid(True)
-
-    return lineH
-
 parser = argparse.ArgumentParser(description='RangeNet logfile analysis')
 parser.add_argument('-f','--ffn_in',help='input file')
 parser.add_argument('-c','--compress',action='store_true',default=False)
@@ -103,40 +61,90 @@ pdf = PdfPages(ffn_pdf)
 
 plt.ion()
 #rsp_list = [101]
-for nodeID in rsp_list :
+for rspID in rsp_list :
 
+    #--- create a mask for this responder
     RA = D['RcmRanges'].ravel()
+    rspID_mask = RA['rspID'] == rspID
 
+    #--- associated Time vector
+    t_vec = RA[rspID_mask]['t_host']
+    t0 = RA['t_host'][0]
+    t_vec = t_vec - t0
+
+    #--- create a figure
     fig = plt.figure(figsize=(18,10))
+    plt.suptitle(fn_main,fontsize=13)
 
-    ## Range Subplot
-    ax0 = plt.subplot(211)
-    plot_ranges(RA,nodeID)
-    plt.suptitle(fn_main,fontsize=14)
-    plt.title('Responder: %d' % (nodeID),fontsize=14)
-    plt.ylabel('Distance (m)',fontsize=14)
-    if 'range_limits' in locals() :
-        ax0.set_ylim(range_limits)
+    #--- isolate ranges
+    r_vec = RA[rspID_mask]['rmeas']
+    print
+    print 'rspID ' + str(rspID)
+    print 'range stats: mean=%5.3f, max=%5.3f, min=%5.3f' %(np.mean(r_vec),max(r_vec),min(r_vec))
 
-    ## SNR Subplot
-    ax1 = plt.subplot(212)
-    #    colors = ['red', 'limegreen', 'blue', 'magenta', 'cyan', 'gray']
-    lineH = []
-    lineH =     [plot_snr(RA, nodeID, pii_array[0]['color'], pii_array[0]['low'], pii_array[0]['high'], pii_array[0]['label'], pass_fail_snr)]
-    lineH.append(plot_snr(RA, nodeID, pii_array[1]['color'], pii_array[1]['low'], pii_array[1]['high'], pii_array[1]['label'], pass_fail_snr))
-    lineH.append(plot_snr(RA, nodeID, pii_array[2]['color'], pii_array[2]['low'], pii_array[2]['high'], pii_array[2]['label'], pass_fail_snr))
-    lineH.append(plot_snr(RA, nodeID, pii_array[3]['color'], pii_array[3]['low'], pii_array[3]['high'], pii_array[3]['label'], pass_fail_snr))
-    plt.ylabel('SNR = 20*log10(Vpeak/noise)',fontsize=14)
+    #---  Range Subplot
+    ax1 = plt.subplot(511)
+    plt.plot(t_vec,r_vec,'b.')
+    plt.grid(True)
+    plt.title('Responder: %d' % (rspID),fontsize=14)
+    plt.ylabel('Rmeas (m)',fontsize=12)
 
-    plt.legend(bbox_to_anchor=(0.,1.03,1.,.102),
-                mode='expand',ncol=len(lineH),fancybox=True,shadow=True)
-#    plt.legend(lineH)
+    ax1.set_ylim([2,16])
 
-    if 'snr_limits' in locals() :
-        ax1.set_ylim(snr_limits)
+    #--- isolate REEs
+    ree_vec = RA[rspID_mask]['ree']
+    print 'ree stats: mean=%5.3f, max=%5.3f, min=%5.3f' %(np.mean(ree_vec),max(ree_vec),min(ree_vec))
 
-#    xmax = max(ax0.get_)
-    ax0.set
+    #---  REE Subplot
+    ax2 = plt.subplot(512)
+    plt.plot(t_vec,ree_vec,'b.')
+    plt.grid(True)
+    plt.ylabel('REE (m)',fontsize=12)
+
+    #--- isolate LED Flags
+    ledflag_vec = RA[rspID_mask]['ledflags']
+    print 'ledflag stats: mean=%5.3f, max=%5.3f, min=%5.3f' % (np.mean(ledflag_vec),max(ledflag_vec),min(ledflag_vec))
+
+    #---  LEDFLAG Subplot
+    ax3 = plt.subplot(513)
+    plt.plot(t_vec,ledflag_vec,'b.')
+    plt.grid(True)
+    plt.ylabel('LED Flags',fontsize=12)
+
+    #---  isolate SNR
+    vpk_vec = RA[rspID_mask]['vpeak']
+    noise_vec = RA[rspID_mask]['noise']
+    snr_vec = 20.*np.log10(np.divide(vpk_vec,noise_vec))
+    print 'snr stats: mean=%5.3f, max=%5.3f, min=%5.3f' % (np.mean(snr_vec),max(snr_vec),min(snr_vec))
+
+    #---  SNR Subplot
+    ax4 = plt.subplot(514)
+    plt.plot(t_vec,snr_vec,'b.')
+    plt.grid(True)
+    plt.ylabel('SNR (m)',fontsize=12)
+
+    #--- isolate delta Times
+    dt_vec = np.diff(t_vec)
+    print 'dt stats: mean=%5.3f, max=%5.3f, min=%5.3f' % (np.mean(dt_vec),max(dt_vec),min(dt_vec))
+
+    #--- plot dT vs T
+    ax5 = plt.subplot(515)
+    plt.plot(t_vec[1:],dt_vec,'b.')
+    plt.grid(True)
+    plt.ylabel('dT (ms)',fontsize=12)
+
+
+    #--- isolate stinkers
+    ree_gt_thresh_mask = (ree_vec > 0.1)
+    t_vec_ree = t_vec[ree_gt_thresh_mask]
+    r_vec_ree = r_vec[ree_gt_thresh_mask]
+
+    #---  overplot range with those with ree > thresh
+    ax1.plot(t_vec_ree,r_vec_ree,'s',markeredgecolor='r',fillstyle='none')
+
+
+
+
 
     plt.draw()
 
